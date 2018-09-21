b981ead42f72b2bb127359feb95b5c1448e52e3e
Dustin Specker <DustinSpecker@DustinSpecker.com>
2018-09-21 17:27:39 -0500

    feat(boot): print HELLO in tty mode

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,3 +1,18 @@
+mov ah, 0x0e ; use tty mode
+
+; print H
+mov al, 'H'
+int 0x10 ; tell video service to print al register (because we're in tty mode)
+
+mov al, 'E'
+int 0x10
+mov al, 'L'
+int 0x10
+mov al, 'L'
+int 0x10
+mov al, 'O'
+int 0x10
+
 jmp $ ; infinite loop
 
 ; fill remaining 510 bytes with 0
